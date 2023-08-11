fdc4bb3fed6d95250ce9f00688150ad508a9fd3d
Rasel Hasan <rasel.hasan@vivacomsolutions.com>
2023-08-11 13:11:44 +0600

    implemented tab view, requesting data with params, added loading/error state, made table component reusuable

---
+++ pagination-practice/src/utils/properties.jsx
@@ -0,0 +1,12 @@
+const launchesHeaders = ["id", "name", "flight_number", "date_local"];
+const rocketHeaders = [
+  "name",
+  "stages",
+  "cost_per_launch",
+  "first_flight",
+  "country",
+  "company",
+];
+const starlinkHeaders = ["id", "launch", "version"];
+
+export { rocketHeaders, starlinkHeaders, launchesHeaders };
